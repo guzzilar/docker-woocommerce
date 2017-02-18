85730f5a27472227ebad8516c75301e21db4f308
guzzilar <iam.nuttanon@gmail.com>
2017-02-18 17:22:25 +0700

    Reimplement Docker WooCommerce

--- build.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-docker-compose -f ./src/wordpress-4.5.3/woocommerce-2.6.4/docker-compose.yml stop
-
-docker-compose -f ./src/wordpress-4.5.3/woocommerce-2.6.4/docker-compose.yml rm -f php
-docker-compose -f ./src/wordpress-4.5.3/woocommerce-2.6.4/docker-compose.yml rm -f mysql
-
-docker-compose -f ./src/wordpress-4.5.3/woocommerce-2.6.4/docker-compose.yml up --build
